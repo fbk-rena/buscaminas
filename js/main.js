07a1cec59fc4cd5c0b1d866b003088842f55cfec
fbk-rena <fbk.rena@gmail.com>
2017-04-04 13:30:50 -0500

    Funciones redactadas, falta ajustar ingreso a arrays

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-alert("hola");
+//alert("hola");
+var bombas= document.getElementsByName("bomba");
+var pistaNum= document.getElementsByName("num");
+var btnVacio= document.getElementsByName("vacio");
+
+console.log(bombas);
+
+function numero(){
+    pistaNum.value="x";
+alert("Bomba cerca CUIDADO!!!");
+}
+
+function vacio(){
+    btnVacio.style= "background-color: #FF9900"; 
+
+}
+function bomba(){
+    alert("Kabooooooom!!!!! GAME OVER");
+}
+
+function play(){
+    alert("Jugemos!!!!");
+}
+
+
+bombas[0].addEventListener( 'click,bomba);
+//pistaNum.addEventListener("click",numero);
+//btnVacio.addEventListener("click",vacio);
+document.addEventListener("click",play);
